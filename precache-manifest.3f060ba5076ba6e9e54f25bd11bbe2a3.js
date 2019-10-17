848d414ed148d0c228a4eeba3dc1c09ebda15a75
Abdul Rauf <raufyy4@gmail.com>
2019-10-17 11:45:41 +0500

    Updates

---
+++ precache-manifest.3f060ba5076ba6e9e54f25bd11bbe2a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "980972f307a02428dc9ff2d04c5dbcfb",
+    "url": "link-to-your-repository/index.html"
+  },
+  {
+    "revision": "079f986b28a094f65eb6",
+    "url": "link-to-your-repository/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "3503c42191b06aa4c24d",
+    "url": "link-to-your-repository/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "079f986b28a094f65eb6",
+    "url": "link-to-your-repository/static/js/2.72972cbd.chunk.js"
+  },
+  {
+    "revision": "3503c42191b06aa4c24d",
+    "url": "link-to-your-repository/static/js/main.d54bb1aa.chunk.js"
+  },
+  {
+    "revision": "107831adc3b0762b615e",
+    "url": "link-to-your-repository/static/js/runtime~main.d116e447.js"
+  }
+]);
